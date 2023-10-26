251808c4f7ed09c8fced9ada06dfc3eaa01caa8d
Ryanjc32 <ryanclashjesus@gmail.com>
2023-10-26 17:05:48 -0300

    Nova Alteração

---
+++ EmpresaABC/MercadoSA/frmWebcam.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace MercadoSA
+{
+    partial class frmWebcam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbbDispositivo = new System.Windows.Forms.ComboBox();
+            this.pctCapturaImagem = new System.Windows.Forms.PictureBox();
+            this.lblDispositivo = new System.Windows.Forms.Label();
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.sfdSalvarImagem = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCapturaImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbbDispositivo
+            // 
+            this.cbbDispositivo.FormattingEnabled = true;
+            this.cbbDispositivo.Location = new System.Drawing.Point(90, 471);
+            this.cbbDispositivo.Name = "cbbDispositivo";
+            this.cbbDispositivo.Size = new System.Drawing.Size(251, 21);
+            this.cbbDispositivo.TabIndex = 0;
+            // 
+            // pctCapturaImagem
+            // 
+            this.pctCapturaImagem.Location = new System.Drawing.Point(58, 71);
+            this.pctCapturaImagem.Name = "pctCapturaImagem";
+            this.pctCapturaImagem.Size = new System.Drawing.Size(455, 299);
+            this.pctCapturaImagem.TabIndex = 1;
+            this.pctCapturaImagem.TabStop = false;
+            // 
+            // lblDispositivo
+            // 
+            this.lblDispositivo.AutoSize = true;
+            this.lblDispositivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDispositivo.Location = new System.Drawing.Point(86, 434);
+            this.lblDispositivo.Name = "lblDispositivo";
+            this.lblDispositivo.Size = new System.Drawing.Size(96, 20);
+            this.lblDispositivo.TabIndex = 2;
+            this.lblDispositivo.Text = "Dispositivo";
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Location = new System.Drawing.Point(615, 81);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(111, 36);
+            this.btnCapturar.TabIndex = 3;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(615, 187);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(111, 34);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(615, 276);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(111, 34);
+            this.btnAbrir.TabIndex = 5;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // frmWebcam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCapturar);
+            this.Controls.Add(this.lblDispositivo);
+            this.Controls.Add(this.pctCapturaImagem);
+            this.Controls.Add(this.cbbDispositivo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmWebcam";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmWebcam";
+            this.Load += new System.EventHandler(this.frmWebcam_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctCapturaImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbDispositivo;
+        private System.Windows.Forms.PictureBox pctCapturaImagem;
+        private System.Windows.Forms.Label lblDispositivo;
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.SaveFileDialog sfdSalvarImagem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAbrir;
+    }
+}
---
+++ EmpresaABC/MercadoSA/frmWebcam.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace MercadoSA
+{
+    public partial class frmWebcam : Form
+    {
+        private bool DeviceExist = false;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource = null;
+
+        public frmWebcam()
+        {
+            InitializeComponent();
+        }
+        public void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+            pctCapturaImagem.Image = img;
+        }
+
+        private void frmWebcam_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);//Filtrando os drives
+                cbbDispositivo.Items.Clear();
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                DeviceExist = true;
+                foreach (FilterInfo device in videoDevices)
+                {
+                    cbbDispositivo.Items.Add(device.Name);
+                }
+                cbbDispositivo.SelectedIndex = 0;
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                cbbDispositivo.Items.Add("Nenhum dispositivo encontrado!");
+            }
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            if (btnCapturar.Text == "Capturar")
+            {
+                if (DeviceExist)
+                {
+                    videoSource = new VideoCaptureDevice(videoDevices[cbbDispositivo.SelectedIndex].MonikerString);
+                    videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+
+
+                    //Encerra sinal da camera.
+                    if (!(videoSource == null))
+                        if(videoSource.IsRunning)
+                    {
+                            videoSource.SignalToStop();
+                            videoSource = null;
+                    }
+                    videoSource.DesiredFrameSize = new Size(160, 120);
+                    videoSource.DesiredFrameRate = 10;
+                    videoSource.Start();
+
+                    btnCapturar.Text = "Gravar";
+                }
+                else
+                {
+                    MessageBox.Show("Nenhum dispositivo encontrado");
+                }
+            }
+            else
+            {
+                if (videoSource.IsRunning)
+                {
+                    //Encerra o sinal da camera.
+                    //note que esse código de bloco se repetiu, você pode criar um método para ele.
+                    if(!(videoSource == null))
+                        if (videoSource.IsRunning)
+                        {
+                            videoSource.SignalToStop();
+                            videoSource = null;
+
+                            //Salva imagem;
+                            sfdSalvarImagem.Filter = "JPEG (*.jpg;*.jpeg;*jpeg;*.jfif) |*.jpg;*jpeg;*jpeg;*.jfif";
+                            DialogResult res = sfdSalvarImagem.ShowDialog();
+                            if(res == DialogResult.OK)
+                            {
+                                pctCapturaImagem.Image.Save(sfdSalvarImagem.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            }
+                        }
+                }
+            }
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog BuscarIMG = new OpenFileDialog();
+            BuscarIMG.InitialDirectory = ("d:\\imagens\\");
+            BuscarIMG.FileName = "Imagem";
+            BuscarIMG.Title = "Procurar Imagem";
+            BuscarIMG.Filter = ("*jpg|*.jpg|*png|*.png|*bmp|*.bmp|*tif|*.tif");
+            BuscarIMG.ShowDialog();
+            pctCapturaImagem.ImageLocation = (BuscarIMG.FileName);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            pctCapturaImagem.Image = null;
+        }
+    }
+}
+    
+
